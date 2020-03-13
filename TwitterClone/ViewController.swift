65bfabc8ffb15b034e96740f99adb89b1c0aa3c6
michaeladudarko <adudarkomichael@gmail.com>
2020-03-13 00:47:33 -0400

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by MICHAEL ADU DARKO on 3/13/20.
+//  Copyright © 2020 Bronzy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
