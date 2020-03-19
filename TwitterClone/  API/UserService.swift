62affbb955bb5245ef5c31b12f7765cae23b707b
michaeladudarko <adudarkomichael@gmail.com>
2020-03-18 21:06:48 -0400

    SDWImage Install & Passing User To FeedController

--- TwitterClone/  API/UserService.swift
+++ TwitterClone/  API/UserService.swift
@@ -13,16 +13,14 @@ struct UserService {
     static let shared = UserService()
     
     //Making API Call to fetch user data from firebase
-    func fetchUser(){
+    func fetchUser(completion: @escaping(User) -> Void){
         guard let uid = Auth.auth().currentUser?.uid else {return}
         
         REF_USERS.child(uid).observeSingleEvent(of: .value) { snapshot in
             guard let dictionary = snapshot.value as? [String: AnyObject] else {return}
             
             let user = User(uid: uid, dictionary: dictionary)
-            
-            print("DEBUG: Username is \(user.username)")
-            print("DEBUG: Fullname is \(user.fullname)")
+            completion(user)
         }
     }
 }
--- TwitterClone/Controllers/FeedController.swift
+++ TwitterClone/Controllers/FeedController.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class FeedController: UIViewController {
        //MARK:- Properties
-     
+    var user: User? {
+        didSet{
+            print("DEBUG: Did set user in feed controller")
+        }
+    }
+    
      //MARK:- Lifecycle
 
      override func viewDidLoad() {
@@ -27,6 +32,15 @@ class FeedController: UIViewController {
         let imageView = UIImageView(image: #imageLiteral(resourceName: "twitter_logo_blue"))
         imageView.contentMode = .scaleAspectFit
         navigationItem.titleView = imageView
+        
+        
+        let profileImageView = UIImageView()
+        profileImageView.backgroundColor = .twitterBlue
+        profileImageView.setDimensions(width: 32, height: 32)
+        profileImageView.layer.cornerRadius = 32 / 2
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: profileImageView)
+        
         
         
     }
