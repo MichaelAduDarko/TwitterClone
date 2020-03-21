2e7444799fcb5c12119f8b74dccbb6c60a0ab4fd
michaeladudarko <adudarkomichael@gmail.com>
2020-03-20 20:37:15 -0400

    Upload Tweet Controller Setup

--- TwitterClone/Controllers/MainTabController.swift
+++ TwitterClone/Controllers/MainTabController.swift
@@ -77,7 +77,9 @@ class MainTabController: UITabBarController {
     }
     //MARK:- selectors
     @objc func actionButtonTapped(){
-        print("1234")
+        let nav = UINavigationController(rootViewController: UploadTweetController())
+        nav.modalPresentationStyle = .fullScreen
+        present(nav, animated: true, completion: nil)
     }
     
     //MARK:- Helpers
